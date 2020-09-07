733c03b0a0665cb62714e3afeff952b7d9966768
Vladislav Likhachev <mymindissharp@gmail.com>
2020-09-07 22:36:45 +0300

    Первый урок

--- objc/objc/main.m
+++ objc/objc/main.m
@@ -7,11 +7,31 @@
 //
 
 #import <Foundation/Foundation.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    int first = 0;
+    int second = 0;
+    
+    printf("Задание 1\n");
+    printf("Введите два числа для выполнения арифметических операций между ними\n");
+    printf("Первое число: ");
+    scanf("%d", &first);
+    printf("Второе число: ");
+    scanf("%d", &second);
+    printf("%d+%d=%d\n", first, second, first+second);
+    printf("%d-%d=%d\n", first, second, first-second);
+    printf("%d*%d=%d\n", first, second, first*second);
+    printf("%d/%d=%f\n", first, second, (float) first/second);
+    
+    printf("\nЗадание 2\n");
+    printf("%d+%d=%d, %d-%d=%d, %d*%d=%d, %d/%d=%d\n",first, second, first+second, first, second, first-second,first, second,first*second,first, second, first/second);
+    
+    printf("\nЗадание 3\n");
+    int randomNumber1 = arc4random_uniform(100);
+    int randomNumber2 = arc4random_uniform(100);
+    int randomNumber3 = arc4random_uniform(100);
+    printf("Среднее арифметическое трех случайных чисел %d %d %d равно %f\n", randomNumber1, randomNumber2, randomNumber3, (float) (randomNumber1 + randomNumber2 + randomNumber3)/3);
+    
     return 0;
 }
