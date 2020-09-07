6c253a7ce335464d661e1f4b59be335d27aae195
Vladislav Likhachev <mymindissharp@gmail.com>
2020-09-07 22:14:15 +0300

    initial commit

---
+++ objc/objc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objc
+//
+//  Created by Владислав Лихачев on 07.09.2020.
+//  Copyright © 2020 Vladislav Likhachev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
