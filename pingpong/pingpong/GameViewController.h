49dcc52112fb60d05661bfc1354fee19cd8aa476
Vladislav Likhachev <mymindissharp@gmail.com>
2020-10-02 00:02:39 +0300

    Добавил код и ассеты

---
+++ pingpong/pingpong/GameViewController.h
@@ -0,0 +1,35 @@
+//
+//  GameViewController.h
+//  pingpong
+//
+//  Created by Владислав Лихачев on 01.10.2020.
+//  Copyright © 2020 Vladislav Likhachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#define HALF_SCREEN_WIDTH SCREEN_WIDTH/2
+#define HALF_SCREEN_HEIGHT SCREEN_HEIGHT/2
+#define MAX_SCORE 6
+
+@interface GameViewController : UIViewController
+
+@property (strong, nonatomic) UIImageView *paddleTop;
+@property (strong, nonatomic) UIImageView *paddleBottom;
+@property (strong, nonatomic) UIView *gridView;
+@property (strong, nonatomic) UIView *ball;
+@property (strong, nonatomic) UITouch *topTouch;
+@property (strong, nonatomic) UITouch *bottomTouch;
+@property (strong, nonatomic) NSTimer * timer;
+@property (nonatomic) float dx;
+@property (nonatomic) float dy;
+@property (nonatomic) float speed;
+@property (strong, nonatomic) UILabel *scoreTop;
+@property (strong, nonatomic) UILabel *scoreBottom;
+
+- (void)config;
+
+@end
+
---
+++ pingpong/pingpong/GameViewController.m
@@ -0,0 +1,60 @@
+//
+//  GameViewController.m
+//  pingpong
+//
+//  Created by Владислав Лихачев on 01.10.2020.
+//  Copyright © 2020 Vladislav Likhachev. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self config];
+}
+
+- (void)config {
+    self.view.backgroundColor = [UIColor colorWithRed:100.0/255.0 green:135.0/255.0 blue:191.0/255.0 alpha:1.0];
+    
+    _gridView = [[UIView alloc] initWithFrame:CGRectMake(0, HALF_SCREEN_HEIGHT - 2, SCREEN_WIDTH, 4)];
+    _gridView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
+    [self.view addSubview:_gridView];
+    
+    _paddleTop = [[UIImageView alloc] initWithFrame:CGRectMake(30, 40, 90, 60)];
+    _paddleTop.image = [UIImage imageNamed:@"paddleTop"];
+    _paddleTop.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:_paddleTop];
+    
+    _paddleBottom = [[UIImageView alloc] initWithFrame:CGRectMake(30, SCREEN_HEIGHT - 90, 90, 60)];
+    _paddleBottom.image = [UIImage imageNamed:@"paddleBottom"];
+    _paddleBottom.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:_paddleBottom];
+    
+    _ball = [[UIView alloc] initWithFrame:CGRectMake(self.view.center.x - 10, self.view.center.y - 10, 20, 20)];
+    _ball.backgroundColor = [UIColor whiteColor];
+    _ball.layer.cornerRadius = 10;
+    _ball.hidden = YES;
+    [self.view addSubview:_ball];
+    
+    _scoreTop = [[UILabel alloc] initWithFrame:CGRectMake(SCREEN_WIDTH - 70, HALF_SCREEN_HEIGHT - 70, 50, 50)];
+    _scoreTop.textColor = [UIColor whiteColor];
+    _scoreTop.text = @"0";
+    _scoreTop.font = [UIFont systemFontOfSize:40.0 weight:UIFontWeightLight];
+    _scoreTop.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:_scoreTop];
+    
+    _scoreBottom = [[UILabel alloc] initWithFrame:CGRectMake(SCREEN_WIDTH - 70, HALF_SCREEN_HEIGHT + 70, 50, 50)];
+    _scoreBottom.textColor = [UIColor whiteColor];
+    _scoreBottom.text = @"0";
+    _scoreBottom.font = [UIFont systemFontOfSize:40.0 weight:UIFontWeightLight];
+    _scoreBottom.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:_scoreBottom];
+}
+
+@end
